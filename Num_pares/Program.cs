e41e60825340a221e4607c4737b8061bc62ac2fe
Mariaocana <79354667+Mariaocana@users.noreply.github.com>
2021-02-27 08:54:17 -0600

    Add files via upload

---
+++ Num_pares/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Num_pares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int num;
+            Console.WriteLine("Ingrese un numero");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num%2==0)
+            {
+                Console.WriteLine("El numero ingresado es par");
+
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado no es par");
+            }
+            Console.ReadKey();
+        }
+    }
+}
